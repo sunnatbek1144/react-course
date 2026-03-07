e1ccc9dfc4a43dcb308a37a3d23b4948926b428e
Sunnatbek <Sunnatbek912@gmail.com>
2026-03-07 23:44:49 +0500

    theme-2 is doen

--- theme-1 (basic react)/src/theme-2/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card() {
-	return (
-		<div className='card'>
-			<img src='https://viaplaceholder.com/200' alt='' className='card-image' />
-		</div>
-	)
-}
-
-export default Card
---
+++ theme-2 (card)/src/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+	return (
+		<div className='card'>
+			<img
+				src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST4JANStfjyL8dCDaI7_7N1Tq_f1OBfEgVhQ&s'
+				alt='png'
+				className='card-image'
+			/>
+			<h2 className='card-title'>Sunnatbek</h2>
+			<p className='card-text'>I'm learn arabic and english laungages</p>
+		</div>
+	)
+}
+export default Card
